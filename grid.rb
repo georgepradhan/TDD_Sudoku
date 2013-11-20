1dbe0fac4a80e96f22ca3ba5094b47fa86154a2c
George Pradhan <kgeorgepradhan@gmail.com>
2013-11-19 21:28:14 -0800

    pull boxes using super_row, super_column

--- grid.rb
+++ grid.rb
@@ -1,6 +1,18 @@
 class Grid
   attr_reader :board, :rows, :columns, :boxes
 
+  SUPER_ROW_COLUMN_BOXES = {
+    [0,0] => 0,
+    [0,1] => 1,
+    [0,2] => 2,
+    [1,0] => 3,
+    [1,1] => 4,
+    [1,2] => 5,
+    [2,0] => 6,
+    [2,1] => 7,
+    [2,2] => 8
+  }
+
   def initialize(board)
     validate board
     @board = board
@@ -26,6 +38,19 @@ class Grid
   end
 
   def create_boxes
+    @boxes = []
+    boxes = Array.new(9) { '' }
+    0.upto(80) do |cell_num|
+      super_column_num = (cell_num % 9) / 3
+      super_row_num = (cell_num / 9) / 3
+      box_num = SUPER_ROW_COLUMN_BOXES[[super_row_num, super_column_num]]
+      boxes[box_num] += self.board[cell_num]
+    end
+    boxes.each { |box| @boxes << Box.new(self, box) }
+  end
+
+  def is_solved?
+    !self.board.include?('0')
   end
 
   private
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe Grid do 
-  context "#initialize" do 
+  let(:grid) { Grid.new('1'*9 + '2'*9 + '3'*9 + '4'*9 +
+                '5'*9 + '6'*9 + '7'*9 + '8'*9 + '9'*9 ) }
 
+  context "#initialize" do 
     it "rejects bad input" do
       expect {
         Grid.new('this is not a grid')
@@ -13,32 +15,50 @@ describe Grid do
       expect(Grid.new('0'*81)).to be_a Grid
     end
 
-    it "generates 9 row objects" do
+    it "generates 9 Rows, 9 Columns, and 9 Boxes" do
       Row.should_receive(:new).exactly(9).times
+      Column.should_receive(:new).exactly(9).times
+      Box.should_receive(:new).exactly(9).times
+
       Grid.new('0'*81) 
     end
   end
 
-  context "#create_rows" do 
-    let(:grid) { Grid.new('1'*9 + '2'*9 + '3'*9 + '4'*9 +
-                  '5'*9 + '6'*9 + '7'*9 + '8'*9 + '9'*9 ) }
-
+  describe "#create_rows" do 
     it "generates row objects appropriately" do 
       grid.rows.first.numbers.should eq '111111111'
       grid.rows.last.numbers.should eq  '999999999'
     end
   end
 
-  context "#create_columns" do 
-    let(:grid) { Grid.new('1'*9 + '2'*9 + '3'*9 + '4'*9 +
-                  '5'*9 + '6'*9 + '7'*9 + '8'*9 + '9'*9 ) }
-
+  describe "#create_columns" do 
     it "generates column objects appropriately" do 
       grid.columns.first.numbers.should eq '123456789'
       grid.columns.last.numbers.should eq  '123456789'
     end
   end
 
+  describe "#create_boxes" do 
+    it "generates box objects appropriately" do 
+      grid.boxes.first.numbers.should eq '111222333'
+      grid.boxes.last.numbers.should eq  '777888999'
+    end
+  end
 
+  describe "#is_solved?" do
+    context "with no 0's" do      
+      let(:grid) { Grid.new('1'*81) } 
+      it "returns true" do 
+        grid.is_solved?.should be_true
+      end
+    end
+
+    context "with some 0's" do 
+      let(:grid) { Grid.new('1'*80+'0') } 
+      it "returns false" do 
+        grid.is_solved?.should be_false
+      end
+    end
+  end
 
 end
